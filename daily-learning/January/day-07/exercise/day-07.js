ba62aa74c00a381607ff46094ab96dcd39f470fa
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-01-07 23:35:05 +0100

    day-07 commit

---
+++ daily-learning/January/day-07/exercise/day-07.js
@@ -0,0 +1,79 @@
+let firstName = `Fagboyo`;
+let lastName = `John`;
+let age = 23;
+let favoriteColor = `blue`;
+let todayDate = "January 7, 2025";
+let birthYear = 2002;
+let likeChocolate = true;
+let almaMater = `Uniabuja`;
+let faveMovie = `spartacus`;
+let country = `Nigeria`;
+
+// Mathematical variables
+let a = 23;
+let b = 25;
+let sum = a + b;
+ console.log(sum);
+
+let length = 10;
+let width = 5;
+let area = length * width;
+
+let fiveYearsTime = age + 5;
+
+let height = `150mm`;
+
+let squareSide = 6;
+let squarePerimeter = squareSide * 4;
+console.log(squarePerimeter);
+
+let distance = 120;
+let time = 7;
+let speed = distance / time;
+console.log(speed);
+
+let price = 4;
+let quantity = 200;
+let totalCost = price * quantity;
+console.log(totalCost);
+
+let radius = 7;
+let circumfrence = 2 * Math.PI * radius * radius;
+console.log(circumfrence);
+
+let num = 4;
+let numSquare = num * num;
+console.log(numSquare);
+
+let hoursWorked = 24;
+let hourlyPay = 5000;
+let totalSalary = hoursWorked * hourlyPay;
+console.log(totalSalary);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let introduction = ` Hello, my name is ${firstName} ${lastName} , i am ${age} years old, i am from ${country}, i am ${height}mm tall.`
+
+let city = "Abuja";
+let formated = `${city}, Nigeria`;
+
+let hobby = "i love listening to good music";
+
+let sentence = `hi my name is ${lastName}, i am ${age} years old, ${hobby}`;
+
+let message = "Welcome to the Javascript world!";
+let greeting = "Hello, ";
+let fullMessage = greeting + message;
+
+let productName = "Shoes";
+let productPrice = 5000;
+let product = `${productName}:N${price}`;
+
+let quote = "The only limit to our realization of tomorrow is our doubts of today.";
+let author = "Franklin D.";
+let fullQuote = `"${quote}" - ${author}`;
+
+let friendName = "Jerry";
+let welcomeMessage = `Hello, ${friendName}! Welcome to ReactBootCamp 2025!`;
+
