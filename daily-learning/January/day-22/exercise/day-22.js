e7601e37adf2eb4173209caa37dc6cafa5b4e8b8
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-01-22 23:23:38 +0100

    day-22 commit

---
+++ daily-learning/January/day-22/exercise/day-22.js
@@ -0,0 +1,13 @@
+// project overview 
+
+            //My Team: Team Alpha
+
+// Create an object of 100 key value pairs 
+// Symptoms and remedies
+
+    // git clone  repo url
+    // git add .
+    // git commit
+    // create branch
+
+    // git push origin branch alpha
