1cac7605b163f2596904561c3b0d3a1a40db9b59
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-01-31 08:01:10 +0100

    day-29 commit

---
+++ daily-learning/January/day-29/exercise/day-29.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Fuctions</title>
+  </head>
+  <body>
+    
+    <script>
+
+      function calculateTax (cost, taxPercentage = 0.1){
+                console.log(cost * taxPercentage);
+            }
+            calculateTax(2000, 0.2);    
+            calculateTax(5000);
+          
+    </script>
+  </body>
+</html>
