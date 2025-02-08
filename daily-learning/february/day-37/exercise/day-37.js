0ffd7bf129055ef0c5426f15cfa6f198a22beba1
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-02-08 10:38:33 +0100

    day-37 commit

---
+++ daily-learning/february/day-37/exercise/day-37.js
@@ -0,0 +1,14 @@
+// given an object 
+
+const student ={
+    name: "John",
+    age: 20,
+    grade: 'A'
+}
+
+
+    for (const keys in student){
+            console.log(keys + `: ` + student[keys]);
+    }
+
+
