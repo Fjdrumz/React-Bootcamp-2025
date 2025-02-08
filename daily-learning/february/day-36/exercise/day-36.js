dcad187cc25682a3c93febd187cb0219af85ca55
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-02-08 10:39:48 +0100

    day-36 commit

---
+++ daily-learning/february/day-36/exercise/day-36.js
@@ -0,0 +1,8 @@
+// write a program that prints even numbers from 2 to 20 using a for loop.
+
+
+for (let i = 2; i <= 20; i++){
+    if(i % 2 === 0){
+        console.log(`${i} is an even number`);
+    }
+}
